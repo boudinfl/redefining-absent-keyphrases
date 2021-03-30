c6b5fa976023a95176323d756270ede0166699b9
Florian Boudin <florian.boudin@univ-nantes.fr>
2021-03-30 16:06:27 +0200

    Update experiments-ntcir-2.sh

--- src/experiments-ntcir-2.sh
+++ src/experiments-ntcir-2.sh
@@ -2,15 +2,15 @@
 
 echo 'Converting documents to TREC format'
 
-# # Create collection for Title+Abstract
-# python3 src/ntcir_to_trec.py --input data/ntcir-2/docs/ \
-#                              --output data/ntcir-2/collections/ntcir-2-t+a/ntcir-2-t+a.gz
+# Create collection for Title+Abstract
+python3 src/ntcir_to_trec.py --input data/ntcir-2/docs/ \
+                             --output data/ntcir-2/collections/ntcir-2-t+a/ntcir-2-t+a.gz
 
 
-# # Create collection for Title+Abstract + all keyphrases
-# python3 src/ntcir_to_trec.py --input data/ntcir-2/docs/ \
-#                              --output data/ntcir-2/collections/ntcir-2-t+a-all/ntcir-2-t+a-all.gz \
-#                              --include_keywords
+# Create collection for Title+Abstract + all keyphrases
+python3 src/ntcir_to_trec.py --input data/ntcir-2/docs/ \
+                             --output data/ntcir-2/collections/ntcir-2-t+a-all/ntcir-2-t+a-all.gz \
+                             --include_keywords
 
 # # Create collections for PRMU keyphrases
 if [[ ! -f "data/ntcir-2/collections/ntcir-2-t+a-mu/ntcir-2-t+a-mu.gz" ]]
